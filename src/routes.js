665373848501cd0fb64b70efd926766261f016b0
gissandrogama <gissandrogama@gmail.com>
2020-01-09 18:11:38 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const OrdemController = require('./controller/OrdemController')
+const EquipamentoController = require('./controller/EquipamentoController')
+
+
 
 const routes = new express.Router()
 
+// Ordem de serviços
 routes.get('/ordems', OrdemController.index)
 routes.post('/ordem', OrdemController.store)
 routes.get('/ordem/:id', OrdemController.show)
 routes.put('/ordem/:id', OrdemController.update)
 routes.delete('/ordem/:id', OrdemController.destroy)
+
+
+// Equipamentos
+routes.post('/equipamentos', EquipamentoController.store)
+
+
 
 module.exports = routes
